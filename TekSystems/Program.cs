197cfdb386aa9a8ef93784e0ff1c9d2af669a88e
gunnarsireus <gunnar.sireus@gmail.com>
2018-09-11 18:52:59 +0200

    Add project files.

---
+++ TekSystems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TekSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
